158bc8caea66e349175391161c08e990a1d034a4
Lydia Wildes <lydiawildes1@gmail.com>
2022-09-29 10:33:13 -0500

    create get and post in server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let guessData = [];
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,6 +12,30 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+app.get('/guesses', (req,res) => {
+  console.log('Client wants the guess data! ❓');
+
+  res.send(guessData);
+})
+
+//When a player submits their guess...
+app.post('/guesses', (req,res) => {
+  console.log('Client made a guess! ❓', req.body);
+
+  //create new variable for client data
+  let newGuesses = req.body
+  //add that new variable to the guessData array 
+  guessData.push(newGuesses);
+  //send all guessData to client
+  res.sendStatus(201);
+
+})
+
+
+
+
+
+
 
 
 app.listen(PORT, () => {
